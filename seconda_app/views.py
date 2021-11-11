c8e0a031ecba0e912de64afa5fe8c4f1d2c5f247
gianluu02 <gianlu.fau02@gmail.com>
2021-11-11 10:40:53 +0100

    Livello 7

--- seconda_app/views.py
+++ seconda_app/views.py
@@ -9,3 +9,12 @@ def es_if(request):
     }
     
     return render(request,"es_if.html",context)
+
+def if_else_elif(request):
+    context={
+        'var1':100,
+        'var2':100.0,
+        'var3':100.50,
+    }
+    
+    return render(request,"if_else_elif.html",context)
