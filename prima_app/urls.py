00efc97afb2b7470a7c52334525cb5e67d97d422
gianluu02 <gianlu.fau02@gmail.com>
2021-11-23 10:00:30 +0100

    104

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -3,10 +3,10 @@ from prima_app.views import homepage,welcome,lista,chi_siamo,variabili,index
 
 app_name="prima_app"
 urlpatterns=[
-    path('homepage',homepage, name='homepage'),
+    path('',homepage, name='homepage'),
     path('welcome',welcome, name='welcome'),
     path('lista',lista, name='lista'),
     path('chi_siamo',chi_siamo, name='chi_siamo'),
     path('variabili', variabili, name='variabili'),
-    path('', index, name='index'),
+    path('index', index, name='index'),
 ]
