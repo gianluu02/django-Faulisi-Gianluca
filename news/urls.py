b922a5226ac53e7b409d9a3a57f862180374b5d0
gianluu02 <gianlu.fau02@gmail.com>
2022-01-13 10:00:48 +0100

    ultimo livello forse

---
+++ news/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import home, ArticoloDetailViewCB, ArticoloListView #articoloDetailView
+
+app_name="news"
+
+urlpatterns = [
+    path("",home,name="homeview"),
+    #path("articoli/<int:pk>", articoloDetailView, name="articolo_detail"),
+    path("articoli/<int:pk>", ArticoloDetailViewCB.as_view(), name="articolo_detail"),
+    path("lista_articoli/", ArticoloListView.as_view(),name="lista_articoli"),
+]
