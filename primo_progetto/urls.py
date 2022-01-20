73f598e4ae55d5ba9956e15896e3c6a58a43a2f8
gianluu02 <gianlu.fau02@gmail.com>
2022-01-20 11:26:06 +0100

    prova_pratica

--- primo_progetto/urls.py
+++ primo_progetto/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
-from prova_pratica1.views import homepage
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("prima_app.urls", namespace="prima_app")),
     path('seconda_app/',include("seconda_app.urls",namespace="seconda_app")),
     path('news/',include("news.urls",namespace="homeview")),
-    path('', homepage, name='homepage')
+    path('prova_pratica1/',include("prova_pratica1.urls", namespace="prova_pratica1"))
+    
  
 ]
