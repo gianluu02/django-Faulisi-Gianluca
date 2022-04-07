93889dc397cf35058ae696757d1826d606e766db
gianluu02 <gianlu.fau02@gmail.com>
2022-04-07 12:00:32 +0200

    product list parte 2

--- prova_pratica1/urls.py
+++ prova_pratica1/urls.py
@@ -1,12 +1,13 @@
 from django.urls.conf import path
-
 from .views import view_b
 from .views import view_c
+from .views import product_list
 
 
 app_name="prova_pratica1"
 
 urlpatterns = [
+    path('product_list/', product_list, name="product_list"),
     path("view_b",view_b,name="view_b"),
     path("view_c",view_c,name="view_c")
 ]
--- prova_pratica1/views.py
+++ prova_pratica1/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from django.http import JsonResponse
 
-# Create your views here.
+
+def product_list(request):
+    
+
+    context={
+        'materie' : ["Matematica","Italiano","Inglese","Storia","Geografia"]
+    }
+    response= JsonResponse(context)
+    return response
+
 def view_b(request):
     context={
         'materie' : ["Matematica","Italiano","Inglese","Storia","Geografia"]
     }
+    
     return render(request, "view_b.html", context)
 
 def view_c(request):
     context={
-        'voti' : {'Lorenzo Cirillo':[("Matematica",9,0),("Italiano",7,3),("Inglese",7.5,4),("Storia",7.5,4),("Geografia",5,7)],
-                  'Andrea Duchini':[("Matematica",8,1),("Italiano",6,1),("Inglese",9.5,0),("Storia",8,2),("Geografia",8,1)],
-                  'Flavio Flavio':[("Matematica",7.5,2),("Italiano",6,2),("Inglese",4,3),("Storia",8.5,2),("Geografia",8,2)]
-                  }
+       'voti' : {'Lorenzo Cirillo':[("Matematica",9,0),("Italiano",7,3),("Inglese",7.5,4),("Storia",7.5,4),("Geografia",5,7)],
+                'Andrea Duchini':[("Matematica",8,1),("Italiano",6,1),("Inglese",9.5,0),("Storia",8,2),("Geografia",8,1)],
+                'Flavio Flavio':[("Matematica",7.5,2),("Italiano",6,2),("Inglese",4,3),("Storia",8.5,2),("Geografia",8,2)]
+                }
     }
     return render(request, "view_c.html", context)
